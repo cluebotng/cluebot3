cb3c8270280e14ae4a2c729b7e5a01c4dd2d60ab
Damian Zaremba <damian@damianzaremba.co.uk>
2016-08-19 23:12:33 +0200

    Enable

--- fabfile.py
+++ fabfile.py
@@ -62,7 +62,7 @@ def _stop():
 def _start():
     print('Starting bot')
     '''
-    sudo(('jstart -N start-cluebot3 -e /dev/null -o /dev/null -mem 12G '
+    sudo(('jstart -N start-cluebot3 -e /dev/null -o /dev/null -mem 15G '
           'php -f %s/cluebot3.php' % DEST_DIR))
     '''
     return
