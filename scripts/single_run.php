267d1daa15ad65aecf6e108d50d40c6cdfaf58a4
Damian Zaremba <damian@damianzaremba.co.uk>
2026-09-03 19:13:02 +0200

    Create scripts dir
    - Move existing scripts
    - Update health check entrypoint
    - Minor cleanup

--- scripts/single_run.php
+++ scripts/single_run.php
@@ -1,32 +1,32 @@
 <?php
 
-    /*
-     * Copyright (C) 2015 Jacobi Carter
-     *
-     * This file is part of ClueBot III.
-     *
-     * ClueBot III is free software: you can redistribute it and/or modify
-     * it under the terms of the GNU General Public License as published by
-     * the Free Software Foundation, either version 2 of the License, or
-     * (at your option) any later version.
-     *
-     * ClueBot III is distributed in the hope that it will be useful,
-     * but WITHOUT ANY WARRANTY; without even the implied warranty of
-     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     * GNU General Public License for more details.
-     *
-     * You should have received a copy of the GNU General Public License
-     * along with ClueBot III.  If not, see <http://www.gnu.org/licenses/>.
-     */
+/*
+ * Copyright (C) 2015 Jacobi Carter
+ *
+ * This file is part of ClueBot III.
+ *
+ * ClueBot III is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ClueBot III is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ClueBot III.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace ClueBot3;
 
-require_once 'lib/bot.php';
-require_once 'lib/config.php';
-require_once 'cluebot3.config.php';
+require_once __DIR__ . '/../lib/bot.php';
+require_once __DIR__ . '/../lib/config.php';
+require_once __DIR__ . '/../cluebot3.config.php';
 
 date_default_timezone_set('Europe/London');
-include 'vendor/autoload.php';
+include __DIR__ . '/../vendor/autoload.php';
 
 Config::init();
 
@@ -43,7 +43,7 @@ $wpi = new \Wikipedia\Index($wph, $wikiLogger);
 $wpapi = new \Wikipedia\Api($wph, $wikiLogger);
 
 if (!$wpapi->login(Config::$user, Config::$pass)) {
-    die('Failed to authenticate\n');
+    die("Failed to authenticate\n");
 }
 
 $target_title = $argv[1] ?? null;
